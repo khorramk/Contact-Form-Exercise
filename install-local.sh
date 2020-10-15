5c9f1250cba2d331ee5e00800dc208cc2bd98d54
khorramk <khorramk.kbsk@gmail.com>
2020-10-15 16:52:19 +0100

    automated install file for local testing

---
+++ install-local.sh
@@ -0,0 +1,19 @@
+echo "Welcome to Contact form exercise!"
+
+echo "This program will install all of the dependencies for your local development"
+
+echo "First composer install"
+
+composer install
+
+echo "composer install finished"
+
+echo "yarn install"
+
+yarn
+
+echo "installation complete"
+
+echo "you can now enjoy coding and developing by running any specific commands"
+
+echo "Thank you. happy coding :)"
