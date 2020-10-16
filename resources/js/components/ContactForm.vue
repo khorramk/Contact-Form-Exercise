1342e5f8395384e8187fb1b815ebc9c7292a890f
khorramk <khorramk.kbsk@gmail.com>
2020-10-16 08:23:04 +0100

    putting validation and checking from the UI

--- resources/js/components/ContactForm.vue
+++ resources/js/components/ContactForm.vue
@@ -5,26 +5,26 @@
                 <div class="form-row">
                     <div class="col-sm-12 col-md-6 col-xl-6">
                         <label for="">First Name</label>
-                        <input type="text" class="form-control" v-model="user.first_name" placeholder="First name">
+                        <input type="text" class="form-control" v-model="user.first_name" placeholder="First name" :required="required">
                     </div>
                     <div class="col-sm-12 col-md-6 col-xl-6">
                         <label for="">Last Name</label>
-                        <input type="text" class="form-control" v-model="user.last_name" placeholder="Last name">
+                        <input type="text" class="form-control" v-model="user.last_name" placeholder="Last name" :required="required">
                     </div>
                 </div>
                 <div class="form-row mt-2">
                     <div class="col-sm-12 col-md-12 col-xl-12">
                         <label for="m-email">Email</label>
-                        <input type="email" class="form-control" v-model="user.email" id="m-email" placeholder="smith@email.com">
+                        <input type="email" class="form-control" v-model="user.email" id="m-email" placeholder="smith@email.com" :required="required">
                     </div>
                 </div>
                 <div class="form-row mt-2">
                     <label for="phone">Phone number</label>
-                    <input class="form-control" type="tel" id="phone" v-model="user.phone" name="phone" pattern="[+][0-9]{3}-[0-9]{3}-[0-9]{4}" required>
+                    <input class="form-control" type="tel" id="phone" v-model="user.phone" name="phone" :required="required">
                 </div>
                 <div class="form-row mb-2">
                     <label for="m-message">Message</label>
-                    <textarea class="form-control" name="m-message" v-model="user.message" id="m-message" cols="30" rows="6"></textarea>
+                    <textarea class="form-control" name="m-message" v-model="user.message" id="m-message" cols="30" rows="6" :required="required"></textarea>
                 </div>
                 <button type="submit" class="btn btn-danger" @click="submit" :disabled="nonClickable">
                     {{ submitted }}
@@ -46,17 +46,26 @@ export default {
                     message: ''
                 },
                 nonClickable: false,
-                submitted: 'Submit'
+                submitted: 'Submit',
+                required: true
             }
         },
         methods: {
             submit(){
-                console.log(this.user);
-                this.nonClickable = true;
-                this.submitted = 'Submitted'
-                axios.post('/api/submit', this.user).then((response) => {
-                    window.location.href = '/message';
-                });
+                this.checkForEmptyField();
+                if (!this.required) {
+                    this.nonClickable = true;
+                    this.submitted = 'Submitted';
+                    axios.post('/api/submit', this.user).then((response) => {
+                        window.location.href = '/message';
+                    });
+                }
+            },
+            checkForEmptyField(){
+                if ( this.user.first_name != '' && this.user.last_name != ''
+                     && this.user.email != '' && this.user.phone != '' && this.user.message != '') {
+                    this.required = false;
+                }
             }
         }
 }
